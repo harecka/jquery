6b331410530dce7c227e524d2f968504ea027085
Karolina <karolina.harecka@gmail.com>
2017-08-26 12:23:51 +0200

    znak dolara

---
+++ js/1-document-ready.js
@@ -0,0 +1,14 @@
+"use strict";
+
+$(function(){
+//    czysty js
+    document.getElementById("first").style.color = "red";
+    
+//    to samo w jquery
+    $("#first").css(
+    {
+        color: "red",
+        background: "yellow"
+    }
+    );
+});
