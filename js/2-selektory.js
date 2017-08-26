19f83f3c7197ae37e9b156b4bf106992a02c7f47
Karolina <karolina.harecka@gmail.com>
2017-08-26 12:34:46 +0200

    comit selektroy

---
+++ js/2-selektory.js
@@ -0,0 +1,16 @@
+"use strict";
+
+
+$function(){
+//    pobieranie lemenetów po znaczniku
+    var paragraphs = $("p");
+    console.log(paragraphs);
+//    
+//    pobieranie elementów po id
+    var paragraphOne = $("#paragraf");
+    console.log(paragraphOne);
+//    
+//    pobieranie po klasie
+    var paragraphTwo=$(".paragraf");
+    console.log(paragraphTwo);
+});
